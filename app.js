cb9466330747eb747aca305c7ff7826a17f4abf5
luckyshanb4 <luckyshanb4@gmail.com>
2021-07-30 10:17:14 +0530

    added default items to mongo DB

--- app.js
+++ app.js
@@ -1,21 +1,52 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const date=require(__dirname+"/date.js");
-
-
-const items =["Buy Foods","Cook Fooda","Eat Foods"];
-let workItems=[];
+const mongoose=require('mongoose');
 
 const app=express();
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+
+mongoose.connect("mongodb://localhost:27017/todoListDB");
+
+const itemSchema=new mongoose.Schema({
+    name:String
+});
+
+const Item = mongoose.model("Item",itemSchema);
+
+const item1=new Item({
+    name:"Welcome to To Do List"
+});
+
+const item2=new Item({
+    name:"Click + to add"
+});
+
+const item3=new Item({
+    name:"Use check box to delete"
+});
+
+const defaultItems=[item1,item2,item3];
+
+Item.insertMany(defaultItems,function(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("added 3 items successfully!");
+    }
+})
+
+
+
 app.get("/",function(req,res){
 
     let day=date.getDay();
 
-    res.render('list',{listTitle:day,newListItem:items});
+    // res.render('list',{listTitle:day,newListItem:items});
 
 
     
@@ -24,10 +55,8 @@ app.get("/",function(req,res){
 app.post("/",function(req,res){
     let item =req.body.newItem;
 
-    // console.log(req.body.list);
-
     if(req.body.list==="Work List"){
-        workItems.push(item);
+        // workItems.push(item);
         res.redirect("/work");
         
     }
@@ -39,7 +68,7 @@ app.post("/",function(req,res){
 })
 
 app.get("/work",function(req,res){
-    res.render("list",{listTitle:"Work List",newListItem:workItems})
+    // res.render("list",{listTitle:"Work List",newListItem:workItems})
 
 })
 
